69882bb0d616742f2bedd8ad3265b315f2488afd
D1348965 <d1348965@o365.fcu.edu.tw>
2026-03-25 21:33:18 +0800

    s

--- config/__init__.py
+++ config/__init__.py
@@ -45,7 +45,7 @@ from .strategy import (
 # 7) Reproducibility: keep DATA_SEED fixed; change it only for new random splits.
 
 # 在這裡改策略 <========================
-STRATEGY_NAME = StrategyName.FEDAVGM
+STRATEGY_NAME = StrategyName.FEDADAGRAD
 DATA_DISTRIBUTION = DataDistribution.IID
 DATASET_NAME = DatasetName.MNIST
 
